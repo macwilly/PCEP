7e6d774939bc5524ffb61f8d650f53a3eaec163c
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2026-01-07 19:31:55 -0500

    More review

---
+++ MODULE_02/02_bit_operators.py
@@ -0,0 +1,22 @@
+# One Bit is 0 or 1
+# One byte is 8 bits
+# Bitwise is almost never used
+# there are 6 bit operators
+
+first_bit = 0
+second_bit = 1
+# AND
+print(first_bit & second_bit)
+
+# OR
+
+print(first_bit | second_bit)
+
+# XOR exclusive or
+print(first_bit ^ second_bit)
+
+# logical negation ~
+
+# Shift operator << >> you are shifting the item over by x bits
+print(1 << 1)
+print(1 >> 1) #makes it the lowest
