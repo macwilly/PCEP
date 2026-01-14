97ba3638840780a091f866e21e82c1157c8d0fb5
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2026-01-14 18:26:39 -0500

    Information about Tuples

---
+++ MODULE_04/06_tuple.py
@@ -0,0 +1,7 @@
+#Tuples are not mutable
+one_tuple = (1,)
+one_tuple_one  = 1,
+multi_tuple = (1,2,3)
+print(one_tuple)
+print(one_tuple_one)
+print(multi_tuple)
---
+++ MODULE_04/08_tuple_operations.py
@@ -0,0 +1,4 @@
+user_data = ('John', 'American', 1964)
+print(len(user_data))
+if 'American' in user_data:
+    print('User is from USA')
